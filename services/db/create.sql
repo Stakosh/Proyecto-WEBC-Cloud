3460cd39e59e009485644a51cc3d8e43a39f6602
Stakosh <soto.javi1@gmail.com>
2024-06-03 15:33:54 -0400

    modificacion raise exception

--- services/db/create.sql
+++ services/db/create.sql
@@ -5,18 +5,18 @@
 DO $$
 BEGIN
     RAISE DEBUG 'Inicio del bloque para creación de la base de datos';
-    PERFORM pg_sleep(1); -- Pausa para simular la operación
+    PERFORM pg_sleep(1); -- Pausa 
     IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'app_dev') THEN
         RAISE INFO 'La base de datos app_dev ya existe.';
     ELSE
         RAISE DEBUG 'Creando la base de datos app_dev';
-        PERFORM pg_sleep(1); -- Pausa para simular la operación
+        PERFORM pg_sleep(1); -- Pausa
         CREATE DATABASE app_dev;
         RAISE INFO 'La base de datos app_dev ha sido creada exitosamente.';
     END IF;
 EXCEPTION
     WHEN OTHERS THEN
-        RAISE ERROR 'Error al crear la base de datos: %', SQLERRM;
+        RAISE EXCEPTION 'Error al crear la base de datos. Código de error: %, Mensaje: %', SQLSTATE, SQLERRM;
 END $$;
 
 
@@ -24,5 +24,5 @@ END $$;
 -- RAISE DEBUG PARA RASTREAR FLUJO DE EJECUCION, INICIO DE BLOQUE Y CREANDO BASE DE DATOS
 -- PERFORM pg_sleep(1): Simula un retardo en la ejecución
 -- RAISE INFO PARA MENSAJES INFORMATIVOS, LA BASE DE DATOS YA EXISTE O HA SIDO CREADA
--- RAISE ERROR PARA INDICAR UN PROBLEMA, ERROR AL CREAR BASE DE DATOS
+-- RAISE EXCEPTION PARA INDICAR UN PROBLEMA, ERROR AL CREAR BASE DE DATOS  (mas completo que el RAISE ERROR)
 --  
